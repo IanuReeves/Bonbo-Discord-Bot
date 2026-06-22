5729fd2655e6c707a9b753512dc03074b76cf87a
IanuReeves <66649143+IanuReeves@users.noreply.github.com>
2026-06-22 04:47:56 -0700

    Delete bot.py
    from scratch!

--- bot.py
+++
@@ -1,9 +0,0 @@
-# Class Libraries
-import discord
-import os
-import random
-from ec2_metadata import ec2_metadata
-
-# Output to make sure the class libraries work.
-print(ec2_metadata.region)
-print(ec2_metadata.instance_id)
